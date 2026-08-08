2bfd1d1c107b237a9d94f1bb413b70582a13ddaa
Zurabishvily <313506723+sarapkinkirill09-create@users.noreply.github.com>
2026-08-08 21:56:42 +0300

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const telegram = window.Telegram?.WebApp
+
+if (telegram) {
+    telegram.ready()
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>,
+  )
